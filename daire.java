021c8c9870cf57a35a8848f9803c9fa6ce404c0d
ezgi02 <104259592+ezgi02@users.noreply.github.com>
2023-02-27 14:53:19 +0300

    Add files via upload

---
+++ daire.java
@@ -0,0 +1,16 @@
+package projeler;
+import java.util.Scanner;
+public class daire {
+		public static void main(String[] args) {
+			Scanner input=new Scanner(System.in);
+			int yaricap;
+			System.out.println("dairenin yarıcapını bulunuz");
+			yaricap=input.nextInt();
+			double daire_cevre=2*3.14*yaricap;
+			double daire_alanı=3.1*yaricap*yaricap;
+			System.out.println("dairenin alanu ve cevresi");
+			System.out.println(daire_alanı);
+			System.out.println(daire_cevre);
+	
+	}
+}
