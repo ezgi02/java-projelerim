af744a8cfc29410cfffa98960542646071d928c7
ezgi02 <104259592+ezgi02@users.noreply.github.com>
2023-02-27 14:48:42 +0300

    Add files via upload

---
+++ taksimetre.java
@@ -0,0 +1,20 @@
+package projeler;
+import java.util.Scanner;
+public class taksimetre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		int km;
+		double toplam;
+		System.out.println("toplam gittiginiz yolun km yaziniz");
+		km=input.nextInt();
+		toplam=10+km*2.20;
+		if(toplam<20) {
+			toplam=20;
+		}
+		
+		System.out.println(toplam);
+	}
+
+}
