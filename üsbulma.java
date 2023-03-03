afebb9ca9fe7ddcd3c3e973e8d67b7633bb8c386
ezgi02 <104259592+ezgi02@users.noreply.github.com>
2023-03-03 22:50:29 +0300

    Add files via upload

---
+++ üsbulma.java
@@ -0,0 +1,20 @@
+package projeler;
+import java.util.Scanner;
+import java.math.*;
+
+public class üsbulma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int i;
+		int sayi=input.nextInt();
+		for(i=0;i<sayi;i++) {
+			System.out.println(Math.pow(4,i));
+		}
+		for(i=0;i<sayi;i++) {
+			System.out.println(Math.pow(5,i));
+		}
+	}
+
+}
