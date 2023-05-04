eb226ee4441871ded60b20e2420b75627a0810c9
ezgi02 <104259592+ezgi02@users.noreply.github.com>
2023-05-04 21:43:19 +0300

    Add files via upload

---
+++ tersucgen.java
@@ -0,0 +1,27 @@
+package projeler;
+import java.util.Scanner;
+
+public class tersucgen {
+
+	public static void main(String[] args) {
+		// Ters ucgen yapma
+		Scanner klavye=new Scanner(System.in);
+        
+        System.out.println("Bir Sayı Giriniz");
+        int n=klavye.nextInt();
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(" ");
+            }   
+            for(int k=2*n-1;k>=(2*i+1);k--){
+                System.out.print("*");
+                
+            }
+                System.out.println();   
+
+        }
+
+	}
+
+}
