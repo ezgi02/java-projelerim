36070cf729706ec45471e25a14b9d6bad088aabb
ezgi02 <104259592+ezgi02@users.noreply.github.com>
2023-06-15 10:40:09 +0300

    Update diziminmax.java

--- diziminmax.java
+++ diziminmax.java
@@ -6,9 +6,9 @@ public class diziminmax {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 int[] list = {15,12,788,1,-1,-778,2,0};
-			Scanner input = new Scanner(System.in);
-			int sayi=input.nextInt();
-	        int min=0,max=0;
+		 Scanner input = new Scanner(System.in);
+		 int sayi=input.nextInt();
+	         int min=0,max=0;
 	        for (int i : list) {
 	            if (i < sayi) {
 	                min = i;
